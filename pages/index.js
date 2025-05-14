6d92be851a200b92297e0868fa3a3a9af1a5672c
hrgr01 <142309930+hrgr01@users.noreply.github.com>
2025-05-14 14:49:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-// Hero + formulär (visuell startsida)
+export default function Home() {
+  return (
+    <div>
+      <h1>Välkommen till Startpilot</h1>
+    </div>
+  );
+}
+/ Hero + formulär (visuell startsida)
