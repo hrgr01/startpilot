ee9f3cb2b4fcfaa716ccb13d4b38e5c8efb05519
hrgr01 <142309930+hrgr01@users.noreply.github.com>
2025-05-14 14:30:29 +0200

    Update invest.js

--- pages/invest.js
+++ pages/invest.js
@@ -1,1 +1,9 @@
-// Investerarformulär + PDF-länk
+// export default function Invest() {
+  return (
+    <div>
+      <h1>Investera i Startpilot 🚀</h1>
+      <p>Vill du vara med på vår resa? Fyll i formuläret så kontaktar vi dig.</p>
+    </div>
+  );
+}
+ + PDF-länk
