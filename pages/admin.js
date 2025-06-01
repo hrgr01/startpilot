1fcc3dafb2f1362edf47ff0a87ab5795237f3af6
hrgr01 <142309930+hrgr01@users.noreply.github.com>
2025-06-01 13:22:17 +0200

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../utils/supabase";
 
 export default function AdminPanel() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
