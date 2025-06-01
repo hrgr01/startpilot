e21c9c02ec1ec0572c7a0e446b409cb6a2ccf766
hrgr01 <142309930+hrgr01@users.noreply.github.com>
2025-06-01 15:53:19 +0200

    Update supabase.js

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,13 +1,9 @@
 // /utils/supabase.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error("❌ Supabase URL eller Anon Key saknas i .env-filen.");
-}
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
 export default supabase;
